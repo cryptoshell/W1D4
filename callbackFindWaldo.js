a089f757da104193a3c6a18ff3a1dab6e837181a
Yelim Oh <yelim.oh1@gmail.com>
2017-06-29 18:48:51 +0000

    Replace for loop with forEach

--- callbackFindWaldo.js
+++ callbackFindWaldo.js
@@ -1,10 +1,10 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  arr.forEach(function(item) {
+    if (item === "Waldo") {
+      found(arr.indexOf(item));   // execute callback
     }
-  }
+  });
 }
 
 function actionWhenFound(index) {

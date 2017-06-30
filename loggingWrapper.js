40b5e83d6bec9b741e744c0df3a7e8b41586d3c0
Yelim Oh <yelim.oh1@gmail.com>
2017-06-30 01:41:14 +0000

    Higher order function logging-wrapper exercise

---
+++ loggingWrapper.js
@@ -0,0 +1,32 @@
+var wrapLog = function (callback, name) {
+
+  return function () {
+  var args = [].slice.call(arguments);
+  var args2 = args.slice(',').join(", ");
+  console.log(name + "(" + args2 + ")" + " => " + callback(arguments[0],
+                                                        arguments[1],arguments[2]));
+  }
+};
+
+
+var area = function (x, y) {
+  return x * y;
+};
+
+
+var logArea = wrapLog(area, "area");
+
+logArea(5, 3); // area(5, 3) => 15
+logArea(3, 2); // area(3, 2) => 6
+
+
+
+var volume = function (x, y, z) {
+  return x * y * z;
+};
+
+
+var logVolume = wrapLog(volume, "volume");
+
+logVolume(5, 3, 2); // volume(5, 3, 2) => 30
+logVolume(3, 2, 4); // volume(3, 2, 4) => 24
